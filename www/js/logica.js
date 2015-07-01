180e45f425d54f418a569165b629d2d14e1678c8
ogranada <oscar.andres.granada@gmail.com>
2015-07-01 18:34:25 -0500

    cambios en aplicación, utilidades javascript agregadas

--- www/js/logica.js
+++ www/js/logica.js
@@ -1,11 +1,50 @@
 
 
-$(document).ready(function(){
+
+function getData(cb){
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://ogranada.github.io/videotienda/data.json');
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            var data = JSON.parse(request.responseText);
+            if(!!cb) cb(data);
+        } else {
+            // We reached our target server, but it returned an error
+        }
+    };
+
+    request.onerror = function() {
+    // There was a connection error of some sort
+    };
+
+    request.send();
+}
+
+function inicio(){
+	getData(cargarDatosAPagina);
+}
+
+$(document).ready(inicio);
+
+
+
+
+
+
+
+////////////////////////////////////////////
+//               IMPORTANTE               //
+////////////////////////////////////////////
+
+
+function cargarDatosAPagina(data){
     for(i in data){
-
+        var dato = data[i];
+        console.log("Dato "+i+":", dato);
     }
-});
-
+}
 
 
 
